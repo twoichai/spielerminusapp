0892a79740b59f9307f5caeabef1954056345133
NiklasEw <niklas.ewes@unity-mail.de>
2024-05-26 12:46:58 +0200

    Added AthleteTest

---
+++ src/test/java/com/example/spielerminusapp/AthleteTest.java
@@ -0,0 +1,15 @@
+package com.example.spielerminusapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@WebMvcTest
+class AthleteTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
